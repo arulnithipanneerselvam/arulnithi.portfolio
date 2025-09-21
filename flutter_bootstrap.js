593ef5faa8b1e1d9d9261e75ef35bbf4b2b89a88
arulnithipanneerselvam <arulnithi.panneerselvam@gmail.com>
2025-09-21 23:15:02 +0530

    Deploy Flutter web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2309652973"
+    serviceWorkerVersion: "3694701106"
   }
 });
